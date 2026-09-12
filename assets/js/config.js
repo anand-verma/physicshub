7e76530b23469ef159fd6207d09ac42c980d2dcc
Anand Verma <akv.vermananad@gmail.com>
2026-09-12 23:52:49 +0530

    config update

--- assets/js/config.js
+++ assets/js/config.js
@@ -11,17 +11,18 @@
  */
 export const CONFIG = {
   // Master application release version
-  version: "1.3.2",
+  version: "1.3.3",
 
   // Granular version tags for all critical assets and datasets
   assets: {
-    styles: "1.3.2",
-    app: "1.3.2",
+    styles: "1.3.3",
+    app: "1.3.3",
     questions: "2026.1",
     syllabus: "1.0",
     analysisIndex: "1.3.0",
     analysisVectors: "1.3.0",
-    analysisWorker: "1.3.0"
+    analysisWorker: "1.3.0",
+    qcabSelection: "1.0"
   },
 
   // HTTP cache policy for fetch requests:
